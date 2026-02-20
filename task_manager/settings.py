0c9d9c2e416eaaf521fe0b3b4dd6254dc2b0d3b1
annetmyshkina <annet.obuchova@gmail.com>
2026-02-20 14:44:24 +0300

    fix databases: add ssl_require

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -66,7 +66,9 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///db.sqlite3',
-        conn_max_age=600
+        conn_max_age=600,
+        conn_health_checks=True,
+        ssl_require=True,
     )
 }
 
