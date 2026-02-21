b181c5fb85868afcc28d399102f24d062ad22278
annetmyshkina <annet.obuchova@gmail.com>
2026-02-21 14:15:00 +0300

    feat: add views and URL configurations HomeView, LoginView, LogoutView, RegisterView, UsersView

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,9 +1,14 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('i18n/', include('django.conf.urls.i18n')),
+    path('', views.HomeView.as_view(), name='home'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('users/', views.UsersView.as_view(), name='users'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,4 +1,29 @@
-from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Приветствие от task_manager!")
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView as AuthLoginView, LogoutView as AuthLogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.models import User
+from django.views.generic.list import ListView
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+class LoginView(AuthLoginView):
+    template_name = 'login.html'
+    next_page = reverse_lazy('home')
+
+class LogoutView(AuthLogoutView):
+    next_page = reverse_lazy('home')
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'register.html'
+    success_url = reverse_lazy('login')
+
+class UsersView(ListView):
+    model = User
+    template_name = 'users.html'
+    context_object_name = 'users'
+    paginate_by = 10
