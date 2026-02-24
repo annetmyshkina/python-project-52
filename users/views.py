ad25fc9472d70cbadc33f2547136253c2dc7a509
annetmyshkina <annet.obuchova@gmail.com>
2026-02-24 12:35:26 +0300

    init new app users

---
+++ users/views.py
@@ -0,0 +1,29 @@
+
+from django.contrib.auth.views import LoginView as AuthLoginView, LogoutView as AuthLogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.models import User
+from django.views.generic.list import ListView
+
+
+class LoginView(AuthLoginView):
+    template_name = 'login.html'
+    next_page = reverse_lazy('home')
+
+
+class LogoutView(AuthLogoutView):
+    next_page = reverse_lazy('home')
+
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'register.html'
+    success_url = reverse_lazy('login')
+
+
+class UsersView(ListView):
+    model = User
+    template_name = 'users.html'
+    context_object_name = 'users'
+    paginate_by = 10
