de61fc66fdd5c1048bf0563f0cacddf6c8e8ffb2
annetmyshkina <annet.obuchova@gmail.com>
2026-02-27 11:35:53 +0300

    feat(urls): setup main URL routing with auth and i18n

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -7,8 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
     path('', views.HomeView.as_view(), name='home'),
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('register/', views.RegisterView.as_view(), name='register'),
-    path('users/', views.UsersView.as_view(), name='users'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('users/', include('users.urls')),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
 ]
