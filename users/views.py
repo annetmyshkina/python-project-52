4e4fa2a4d70a6d7652aa67f33d12a10699c38d78
annetmyshkina <annet.obuchova@gmail.com>
2026-02-27 11:39:44 +0300

    feat(users.views): add CRUD class-based views

--- users/views.py
+++ users/views.py
@@ -1,29 +1,64 @@
 
-from django.contrib.auth.views import LoginView as AuthLoginView, LogoutView as AuthLogoutView
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import logout
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import UpdateView, DeleteView
 from django.views.generic.edit import CreateView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.contrib.auth.models import User
 from django.views.generic.list import ListView
+from .forms import CustomUserCreationForm, CustomUserChangeForm, UserDeleteForm
+from django.contrib import messages
 
 
-class LoginView(AuthLoginView):
-    template_name = 'login.html'
-    next_page = reverse_lazy('home')
-
-
-class LogoutView(AuthLogoutView):
-    next_page = reverse_lazy('home')
-
-
-class RegisterView(CreateView):
-    form_class = UserCreationForm
-    template_name = 'register.html'
+class UserCreateView(SuccessMessageMixin, CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'users/user_create.html'
     success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        messages.success(self.request, 'User successfully registered!')
+        return super().form_valid(form)
+
+
+class UserUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = User
+    form_class = CustomUserChangeForm
+    template_name = 'users/user_update.html'
+    success_url = reverse_lazy('users_list')
+
+    def test_func(self):
+        return self.get_object() == self.request.user
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['instance'] = self.object
+        return kwargs
+
+
+class UserDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = User
+    form_class = UserDeleteForm
+    template_name = 'users/user_delete.html'
+    success_url = reverse_lazy('home')
+
+    def test_func(self):
+        return self.get_object() == self.request.user
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['instance'] = self.object
+        return kwargs
+
+    def form_valid(self, form):
+        messages.success(self.request, 'The user has been successfully deleted!')
+        self.object.delete()
+        logout(self.request)
+        return super().form_valid(form)
 
 
 class UsersView(ListView):
     model = User
-    template_name = 'users.html'
+    template_name = 'users/users_list.html'
     context_object_name = 'users'
     paginate_by = 10
