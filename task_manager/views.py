f3e16e24989e7385881ce59b4e9ea8d4b9b4120f
annetmyshkina <annet.obuchova@gmail.com>
2026-02-27 11:38:38 +0300

    feat(views): add CustomLoginView and CustomLogoutView

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,24 @@
-
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib import messages
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        messages.success(self.request, 'You have successfully logged in!')
+        return reverse_lazy('home')
+
+
+class CustomLogoutView(LogoutView):
+    next_page = reverse_lazy('home')
+
+    def dispatch(self, request, *args, **kwargs):
+        messages.success(self.request, 'You have successfully logged out!')
+        return super().dispatch(request, *args, **kwargs)
