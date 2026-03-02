5fc33f2f0cca3e76921a6ed6b7d5e380d0283f41
annetmyshkina <annet.obuchova@gmail.com>
2026-03-02 14:26:49 +0300

    add views.py

--- statuses/views.py
+++ statuses/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+from .models import Statuses
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.utils.translation import gettext_lazy as _
 
-# Create your views here.
+from .forms import StatusForm
+
+
+class StatusCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    model = Statuses
+    form_class = StatusForm
+    template_name = "statuses/status_create.html"
+    success_url = reverse_lazy("statuses")
+
+
+class StatusUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    model = Statuses
+    form_class = StatusForm
+    template_name = "statuses/status_update.html"
+    success_url = reverse_lazy("statuses")
+    success_message = _('Status successfully updated')
+
+
+class StatusDeleteView(LoginRequiredMixin, DeleteView):
+    model = Statuses
+    template_name = "statuses/status_delete.html"
+    success_url = reverse_lazy("statuses")
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+
+        if self.object.tasks.exists():
+            count = self.object.tasks.count()
+            messages.error(
+                request,
+                _('Cannot delete status "%(name)s". It is used in %(count)d tasks.') % {
+                    'name': self.object.name,
+                    'count': count
+                }
+            )
+            return redirect('statuses')
+
+        self.object.delete()
+        messages.success(
+            request,
+            _('Status "%(name)s" deleted successfully') % {'name': self.object.name}
+        )
+        return redirect('statuses')
+
+
+class StatusesView(LoginRequiredMixin, ListView):
+    model = Statuses
+    template_name = "statuses/statuses_list.html"
+    context_object_name = "statuses"
+    ordering = ["name"]
+    paginate_by = 10
