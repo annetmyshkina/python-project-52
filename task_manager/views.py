dea60fd6e654007ddfe51848bd12248f25b3c0aa
annetmyshkina <annet.obuchova@gmail.com>
2026-03-06 19:29:13 +0300

    fix: i18n translations

--- task_manager/views.py
+++ task_manager/views.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
-
+from django.utils.translation import gettext_lazy as _
 
 class HomeView(TemplateView):
     template_name = "home.html"
@@ -13,7 +13,7 @@ class CustomLoginView(LoginView):
     redirect_authenticated_user = True
 
     def get_success_url(self):
-        messages.success(self.request, "You have successfully logged in.")
+        messages.success(self.request, _("You have successfully logged in."))
         return reverse_lazy("home")
 
 
@@ -21,5 +21,5 @@ class CustomLogoutView(LogoutView):
     next_page = reverse_lazy("home")
 
     def dispatch(self, request, *args, **kwargs):
-        messages.success(self.request, "You have successfully logged out.")
+        messages.success(self.request, _("You have successfully logged out."))
         return super().dispatch(request, *args, **kwargs)
