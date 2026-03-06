d3c83023c9e3f34e0c2904979d8af6c0150fedb2
annetmyshkina <annet.obuchova@gmail.com>
2026-03-06 19:31:19 +0300

    check ruff

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,8 +1,9 @@
 from django.contrib import messages
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView
 from django.utils.translation import gettext_lazy as _
+from django.views.generic import TemplateView
+
 
 class HomeView(TemplateView):
     template_name = "home.html"
